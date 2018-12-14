addd881fbb049930eab5a342b21876c097cda81c
Howard Sun <howardsun@csie.io>
2018-12-15 01:51:13 +0800

    Update

--- IMKit.framework/Headers/IMKit-Swift.h
+++ IMKit.framework/Headers/IMKit-Swift.h
@@ -475,11 +475,11 @@ SWIFT_CLASS("_TtC5IMKit27IMImageViewerViewController")
 @end
 
 
+
+
 @interface IMImageViewerViewController (SWIFT_EXTENSION(IMKit)) <UIScrollViewDelegate>
 - (void)scrollViewDidEndDecelerating:(UIScrollView * _Nonnull)scrollView;
 @end
-
-
 
 @class UICollectionView;
 

7e0ecbd36c792209a8e050f11117b9aea23e740a
Howard Sun <howardsun@csie.io>
2019-08-01 23:49:16 +0800

    Update to 0.156.0
    Remove reference of IMUser from IMMessage & migrate it to senderID
    Fix editing message issues
    Update reconnectWaitMax to 5 secs

--- IMKit.framework/Headers/IMKit-Swift.h
+++ IMKit.framework/Headers/IMKit-Swift.h
@@ -612,6 +612,16 @@ SWIFT_CLASS("_TtC5IMKit30IMLinkPreviewSectionController")
 @end
 
 
+SWIFT_CLASS("_TtC5IMKit10IMLiteUser")
+@interface IMLiteUser : RealmSwiftObject
+@property (nonatomic, readonly, copy) NSString * _Nonnull id;
+@property (nonatomic, readonly, copy) NSString * _Nonnull nickname;
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithValue:(id _Nonnull)value schema:(RLMSchema * _Nonnull)schema OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithRealm:(RLMRealm * _Nonnull)realm schema:(RLMObjectSchema * _Nonnull)schema OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
 SWIFT_CLASS("_TtC5IMKit10IMLocation")
 @interface IMLocation : RealmSwiftObject
 @property (nonatomic, readonly, copy) NSString * _Nonnull address;
@@ -669,7 +679,6 @@ SWIFT_CLASS("_TtC5IMKit16IMMemberProperty")
 - (nonnull instancetype)initWithRealm:(RLMRealm * _Nonnull)realm schema:(RLMObjectSchema * _Nonnull)schema OBJC_DESIGNATED_INITIALIZER;
 @end
 
-@class IMUser;
 @class IMSystemEvent;
 @class IMResponseObject;
 
@@ -681,7 +690,7 @@ SWIFT_CLASS("_TtC5IMKit9IMMessage")
 @property (nonatomic, readonly, copy) NSString * _Nonnull stickerID;
 @property (nonatomic, readonly, copy) NSDate * _Nonnull createTime;
 @property (nonatomic, readonly, copy) NSDate * _Nonnull updateTime;
-@property (nonatomic, readonly, strong) IMUser * _Nullable sender;
+@property (nonatomic, readonly, copy) NSString * _Nonnull senderID;
 @property (nonatomic, readonly, strong) IMImage * _Nullable image;
 @property (nonatomic, readonly, strong) IMFile * _Nullable file;
 @property (nonatomic, readonly, strong) IMSystemEvent * _Nullable systemEvent;
@@ -708,16 +717,16 @@ SWIFT_CLASS("_TtC5IMKit36IMMessageActionPopoverViewController")
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
 @end
 
-@class UITableView;
-
-@interface IMMessageActionPopoverViewController (SWIFT_EXTENSION(IMKit)) <UITableViewDelegate>
-- (void)tableView:(UITableView * _Nonnull)tableView didSelectRowAtIndexPath:(NSIndexPath * _Nonnull)indexPath;
-@end
-
 @class UIPresentationController;
 
 @interface IMMessageActionPopoverViewController (SWIFT_EXTENSION(IMKit)) <UIPopoverPresentationControllerDelegate>
 - (UIModalPresentationStyle)adaptivePresentationStyleForPresentationController:(UIPresentationController * _Nonnull)controller SWIFT_WARN_UNUSED_RESULT;
+@end
+
+@class UITableView;
+
+@interface IMMessageActionPopoverViewController (SWIFT_EXTENSION(IMKit)) <UITableViewDelegate>
+- (void)tableView:(UITableView * _Nonnull)tableView didSelectRowAtIndexPath:(NSIndexPath * _Nonnull)indexPath;
 @end
 
 @class UITableViewCell;
@@ -800,7 +809,7 @@ SWIFT_CLASS("_TtC5IMKit13IMProgessView")
 SWIFT_CLASS("_TtC5IMKit16IMResponseObject")
 @interface IMResponseObject : RealmSwiftObject
 @property (nonatomic, readonly, copy) NSString * _Nonnull id;
-@property (nonatomic, readonly, strong) IMUser * _Nullable sender;
+@property (nonatomic, readonly, copy) NSString * _Nonnull senderID;
 @property (nonatomic, readonly, copy) NSString * _Nonnull text;
 @property (nonatomic, readonly, copy) NSString * _Nullable stickerID;
 + (NSArray<NSString *> * _Nonnull)ignoredProperties SWIFT_WARN_UNUSED_RESULT;
@@ -1824,6 +1833,16 @@ SWIFT_CLASS("_TtC5IMKit30IMLinkPreviewSectionController")
 @end
 
 
+SWIFT_CLASS("_TtC5IMKit10IMLiteUser")
+@interface IMLiteUser : RealmSwiftObject
+@property (nonatomic, readonly, copy) NSString * _Nonnull id;
+@property (nonatomic, readonly, copy) NSString * _Nonnull nickname;
+- (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithValue:(id _Nonnull)value schema:(RLMSchema * _Nonnull)schema OBJC_DESIGNATED_INITIALIZER;
+- (nonnull instancetype)initWithRealm:(RLMRealm * _Nonnull)realm schema:(RLMObjectSchema * _Nonnull)schema OBJC_DESIGNATED_INITIALIZER;
+@end
+
+
 SWIFT_CLASS("_TtC5IMKit10IMLocation")
 @interface IMLocation : RealmSwiftObject
 @property (nonatomic, readonly, copy) NSString * _Nonnull address;
@@ -1881,7 +1900,6 @@ SWIFT_CLASS("_TtC5IMKit16IMMemberProperty")
 - (nonnull instancetype)initWithRealm:(RLMRealm * _Nonnull)realm schema:(RLMObjectSchema * _Nonnull)schema OBJC_DESIGNATED_INITIALIZER;
 @end
 
-@class IMUser;
 @class IMSystemEvent;
 @class IMResponseObject;
 
@@ -1893,7 +1911,7 @@ SWIFT_CLASS("_TtC5IMKit9IMMessage")
 @property (nonatomic, readonly, copy) NSString * _Nonnull stickerID;
 @property (nonatomic, readonly, copy) NSDate * _Nonnull createTime;
 @property (nonatomic, readonly, copy) NSDate * _Nonnull updateTime;
-@property (nonatomic, readonly, strong) IMUser * _Nullable sender;
+@property (nonatomic, readonly, copy) NSString * _Nonnull senderID;
 @property (nonatomic, readonly, strong) IMImage * _Nullable image;
 @property (nonatomic, readonly, strong) IMFile * _Nullable file;
 @property (nonatomic, readonly, strong) IMSystemEvent * _Nullable systemEvent;
@@ -1920,16 +1938,16 @@ SWIFT_CLASS("_TtC5IMKit36IMMessageActionPopoverViewController")
 - (nullable instancetype)initWithCoder:(NSCoder * _Nonnull)aDecoder OBJC_DESIGNATED_INITIALIZER;
 @end
 
-@class UITableView;
-
-@interface IMMessageActionPopoverViewController (SWIFT_EXTENSION(IMKit)) <UITableViewDelegate>
-- (void)tableView:(UITableView * _Nonnull)tableView didSelectRowAtIndexPath:(NSIndexPath * _Nonnull)indexPath;
-@end
-
 @class UIPresentationController;
 
 @interface IMMessageActionPopoverViewController (SWIFT_EXTENSION(IMKit)) <UIPopoverPresentationControllerDelegate>
 - (UIModalPresentationStyle)adaptivePresentationStyleForPresentationController:(UIPresentationController * _Nonnull)controller SWIFT_WARN_UNUSED_RESULT;
+@end
+
+@class UITableView;
+
+@interface IMMessageActionPopoverViewController (SWIFT_EXTENSION(IMKit)) <UITableViewDelegate>
+- (void)tableView:(UITableView * _Nonnull)tableView didSelectRowAtIndexPath:(NSIndexPath * _Nonnull)indexPath;
 @end
 
 @class UITableViewCell;
@@ -2012,7 +2030,7 @@ SWIFT_CLASS("_TtC5IMKit13IMProgessView")
 SWIFT_CLASS("_TtC5IMKit16IMResponseObject")
 @interface IMResponseObject : RealmSwiftObject
 @property (nonatomic, readonly, copy) NSString * _Nonnull id;
-@property (nonatomic, readonly, strong) IMUser * _Nullable sender;
+@property (nonatomic, readonly, copy) NSString * _Nonnull senderID;
 @property (nonatomic, readonly, copy) NSString * _Nonnull text;
 @property (nonatomic, readonly, copy) NSString * _Nullable stickerID;
 + (NSArray<NSString *> * _Nonnull)ignoredProperties SWIFT_WARN_UNUSED_RESULT;
